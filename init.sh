be9a4fc58f4b3d5425692e5a78a4fabbed6ada0e
Quentin <quentin.come@etu.univ-orleans.fr>
2021-01-16 02:34:43 +0100

    m

--- init.sh
+++ init.sh
@@ -7,3 +7,8 @@ echo Droplet: $HOSTNAME, IP Address: $PUBLIC_IPV4 > /usr/share/nginx/html/index.
 
 sudo curl -o install_docker.sh https://get.docker.com/
 bash install_docker.sh
+sudo usermod -aG docker root
+
+docker network create prestashop-net
+docker run -ti --name some-mysql --network prestashop-net -e MYSQL_ROOT_PASSWORD=admin -p 3307:3306 -d mysql:5.7
+docker run -ti --name some-prestashop --network prestashop-net -e DB_SERVER=some-mysql -p 8080:80 -d prestashop/prestashop
