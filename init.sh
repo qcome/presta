2ac3d097b4814902946673d1804ba8b3033cdb22
Quentin <quentin.come@etu.univ-orleans.fr>
2021-01-16 02:10:14 +0100

    m

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ apt-get -y update
 apt-get -y install nginx
 export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
 export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
-echo Droplet: $HOSTNAME, IP Address: $PUBLIC_IPV4 > /usr/share/nginx/html/index.html
+echo Droplet: $HOSTNAME, IP Address: $PUBLIC_IPV4 > /var/www/html/index.nginx-debian.html
