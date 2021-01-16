f9008f2f57858dc9abb9c4631123f3f2e0bbb121
Quentin <quentin.come@etu.univ-orleans.fr>
2021-01-16 02:26:38 +0100

    m

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 apt-get -y update
-apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+
+export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
+export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+echo Droplet: $HOSTNAME, IP Address: $PUBLIC_IPV4 > /usr/share/nginx/html/index.html
+
+sudo curl -o install_docker.sh https://get.docker.com/
+bash install_docker.sh
