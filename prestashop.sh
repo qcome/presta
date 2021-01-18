391960ed8f4a91790ee756e331a281ab04db62be
Quentin <quentin.come@etu.univ-orleans.fr>
2021-01-18 21:32:00 +0100

    p

--- prestashop.sh
+++ prestashop.sh
@@ -20,7 +20,7 @@ else
         mkdir -p archives $app_folder
         unzip -n -q archives/prestashop_$ps_version.zip -d $app_folder
 
-        unzip -n -q $app_folder/prestashop.zip
+        unzip -n -q $app_folder/prestashop.zip -d $app_folder
         rm -rf $app_folder/prestashop.zip
     
         #chown www-data:www-data -R $folder/prestashop/
