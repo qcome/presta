1ca85f9aeb244c082ff47e1f1a7e0c9c984a024d
Quentin <quentin.come@etu.univ-orleans.fr>
2021-01-18 21:14:05 +0100

    p

--- prestashop.sh
+++ prestashop.sh
@@ -4,20 +4,23 @@ if [ -z "$1" ]; then
 else
     # Conf variables transmises au docker-compose
     app_name=$1
-    ps_version=$ps_version
-    folder=sites/$app_name
+    app_folder=sites/$app_name
+    ps_version=1.7.7.1
+    
 
     export APP_NAME=$app_name
 
     if [ ! -d sites/$app_name ]; then
+        echo https://github.com/PrestaShop/PrestaShop/releases/download/$ps_version/prestashop_$ps_version.zip
         if [ ! -f archives/prestashop_$ps_version.zip ]; then
             curl -fsSL https://github.com/PrestaShop/PrestaShop/releases/download/$ps_version/prestashop_$ps_version.zip -o archives/prestashop/prestashop_$ps_version.zip
         fi
-        unzip -d $folder archives/prestashop_$ps_version.zip
-        rm $folder/prestashop.zip
 
-        unzip -n -q $folder/prestashop.zip -d $folder/prestashop
-        rm -rf $folder/prestashop.zip
+        unzip -d $app_folder archives/prestashop_$ps_version.zip
+        rm $app_folder/prestashop.zip
+
+        unzip -n -q $app_folder/prestashop.zip -d $app_folder/prestashop
+        rm -rf $app_folder/prestashop.zip
     
         #chown www-data:www-data -R $folder/prestashop/
         #cp -n -R -p $folder/prestashop/* /var/www/html
