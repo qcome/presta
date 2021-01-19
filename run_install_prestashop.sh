3d286246cebe876aea7cd9df2643105b5470e62b
Quentin <quentin.come@etu.univ-orleans.fr>
2021-01-19 21:09:57 +0100

    e

--- run_install_prestashop.sh
+++ run_install_prestashop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-php ./install/index_cli.php 
-    --domain=${PS_DOMAIN} 
-    --db_server=${MYSQL_SERVER} 
-    --db_name=${MYSQL_DATABASE} 
-    --db_user=${MYSQL_USER} 
-    --db_password=${MYSQL_PASSWORD} 
-    --email=${PS_ADMIN_EMAIL} 
-    --password=${PS_ADMIN_PASSWORD} 
-    --language=${PS_LANGUAGE} 
-    --country=${PS_COUNTRY} 
-    && apachectl -D FOREGROUND
+php ./install/index_cli.php \
+--domain=${PS_DOMAIN} \
+--db_server=${MYSQL_SERVER} \
+--db_name=${MYSQL_DATABASE} \
+--db_user=${MYSQL_USER} \
+--db_password=${MYSQL_PASSWORD} \
+--email=${PS_ADMIN_EMAIL} \
+--password=${PS_ADMIN_PASSWORD} \
+--language=${PS_LANGUAGE} \
+--country=${PS_COUNTRY} \
+&& apachectl -D FOREGROUND
