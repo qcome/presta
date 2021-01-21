35bd7e9b7799c00ea7f6fe5834ecf034b2fa792d
Quentin <quentin.come@etu.univ-orleans.fr>
2021-01-21 21:42:35 +0100

    m

--- init-db.sh
+++ init-db.sh
@@ -1,3 +1,6 @@
-GRANT ALL ON *.* TO 'ps_user'@'%';
-FLUSH PRIVILEGES;
-EXIT;
+
+mysql <<EOF
+GRANT ALL PRIVILEGES ON 'prestashop' TO 'mysqld'
+GRANT ALL PRIVILEGES ON 'prestashop' TO 'username'
+FlUSH PRIVILEGES;
+EOF
