3e74f96177edcb65c499d1c85efdf4146c0d2784
Quentin <quentin.come@etu.univ-orleans.fr>
2021-02-14 01:24:32 +0100

    m

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 # Wait for the db to be up, if we know where it is.
 if [ -n "$DB_HOST" ]; then
-  /tmp/wait-for-it.sh --timeout=60 --strict "$DB_HOST:${DB_PORT:-3306}"
+  /tmp/wait-for-it.sh --timeout=60 --strict "$DB_HOST:${DB_PORT:-3306}" -- /tmp/run_install_prestashop.sh
 fi
 
 # Run the main container command.
