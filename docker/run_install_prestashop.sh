d1bdae491654f515bdb5b2c8bfc9cbfe91971992
Quentin <quentin.come@etu.univ-orleans.fr>
2021-03-06 22:04:27 +0100

    m

--- docker/run_install_prestashop.sh
+++ docker/run_install_prestashop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mv /var/www/html/install /var/www/html/installed
 echo "Running prestashop install..."
-runuser -g www-data -u www-data -- php -d memory_limit=-1 /var/www/html/install/index_cli.php \
+runuser -g www-data -u www-data -- php -d memory_limit=-1 /var/www/html/installed/index_cli.php \
     --domain=${PS_DOMAIN} \
     --db_server=${MYSQL_SERVER} \
     --db_name=${MYSQL_DATABASE} \
@@ -15,10 +15,12 @@ runuser -g www-data -u www-data -- php -d memory_limit=-1 /var/www/html/install/
 rm -r /var/www/html/installed \
 mv /var/www/html/admin /var/www/html/${PS_FOLDER_ADMIN}
 mv /tmp/phppsinfo.php /var/www/html
+cd /var/www/html
 git config --global user.email "quentin.come4@gmail.com"
 git config --global user.name "Quentin"
 git init
-git commit -am "init"
+git add .
+git commit -m "init"
 git push --set-upstream git@gitlab.com:qcome-prestashop/$1.git master
 apachectl -D FOREGROUND
 #&& git init \
