abc997a826b86e37539fc7b8d050a2bc2aae58de
Quentin <quentin.come@etu.univ-orleans.fr>
2021-03-06 20:31:30 +0100

    m

--- docker/run_install_prestashop.sh
+++ docker/run_install_prestashop.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-echo "Running prestashop install..." \
-&& runuser -g www-data -u www-data -- php -d memory_limit=-1 /var/www/html/install/index_cli.php \
---domain=${PS_DOMAIN} \
---db_server=${MYSQL_SERVER} \
---db_name=${MYSQL_DATABASE} \
---db_user=${MYSQL_USER} \
---db_password=${MYSQL_PASSWORD} \
---email=${PS_ADMIN_EMAIL} \
---password=${PS_ADMIN_PASSWORD} \
---language=${PS_LANGUAGE} \
---country=${PS_COUNTRY} \
---db_create=${PS_DB_CREATE} \
-&& rm -rf $app_folder/install \
-&& mv /var/www/html/admin /var/www/html/${PS_FOLDER_ADMIN} \
-&& mv /tmp/phppsinfo.php /var/www/html \
-&& apachectl -D FOREGROUND
+echo "Running prestashop install..."
+runuser -g www-data -u www-data -- php -d memory_limit=-1 /var/www/html/install/index_cli.php \
+    --domain=${PS_DOMAIN} \
+    --db_server=${MYSQL_SERVER} \
+    --db_name=${MYSQL_DATABASE} \
+    --db_user=${MYSQL_USER} \
+    --db_password=${MYSQL_PASSWORD} \
+    --email=${PS_ADMIN_EMAIL} \
+    --password=${PS_ADMIN_PASSWORD} \
+    --language=${PS_LANGUAGE} \
+    --country=${PS_COUNTRY} \
+    --db_create=${PS_DB_CREATE} \
+rm -rf $app_folder/install
+mv /var/www/html/admin /var/www/html/${PS_FOLDER_ADMIN}
+mv /tmp/phppsinfo.php /var/www/html
+apachectl -D FOREGROUND
 #&& git init \
 #&& git commit -am "init" \
