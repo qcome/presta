2dd08a21ae224f5fda85c31c8dbb477f5d24d63e
Quentin <quentin.come@etu.univ-orleans.fr>
2021-03-06 19:48:51 +0100

    m

--- docker/run_install_prestashop.sh
+++ docker/run_install_prestashop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mv /var/www/html/install /var/www/html/installed \
+&& echo "${APP_NAME}"  \
 && echo "Running prestashop install..." \
 && runuser -g www-data -u www-data -- php -d memory_limit=-1 /var/www/html/installed/index_cli.php \
 --domain=${PS_DOMAIN} \
@@ -15,5 +16,4 @@ mv /var/www/html/install /var/www/html/installed \
 && rm -r /var/www/html/installed \
 && mv /var/www/html/admin /var/www/html/${PS_FOLDER_ADMIN} \
 && mv /tmp/phppsinfo.php /var/www/html \
-&& echo "${APP_NAME}"  \
 && apachectl -D FOREGROUND
