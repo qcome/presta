0fde77318b367c96b011402ab36257713b1b14a1
Quentin <quentin.come@etu.univ-orleans.fr>
2021-03-13 21:25:52 +0100

    m

--- docker/nginx/docker_entrypoint.sh
+++ docker/nginx/docker_entrypoint.sh
@@ -2,6 +2,8 @@
 
 # Abort on any error (including if wait-for-it fails).
 set -e
+echo "TESTTTT"
+echo "$@"
 
 # Wait for the nginx app to be up, if we know where it is.
 if [ -n "$APP_HOST" ]; then
