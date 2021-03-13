acd2b7f976e9bf125e7acde5851a0a7b03d0f10f
Quentin <quentin.come@etu.univ-orleans.fr>
2021-03-13 21:33:48 +0100

    m

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -3,6 +3,7 @@
 # Abort on any error (including if wait-for-it fails).
 set -e
 echo "DOUZZZE"
+echo "$@"
 # Wait for the db to be up, if we know where it is.
 if [ -n "$DB_HOST" ]; then
   /tmp/wait-for-it.sh --timeout=60 --strict "$DB_HOST:${DB_PORT:-3306}"
