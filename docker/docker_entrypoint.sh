dbfcc5f979406f8252e7821fce6ac1d5622e28b9
Quentin <quentin.come@etu.univ-orleans.fr>
2021-03-13 21:18:40 +0100

    m

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 # Wait for the db to be up, if we know where it is.
 if [ -n "$DB_HOST" ]; then
-  /tmp/wait-for-it.sh --timeout=60 --strict "$DB_HOST:${DB_PORT:-3306}" -- /tmp/run_install_prestashop.sh
+  /tmp/wait-for-it.sh --timeout=60 --strict "$DB_HOST:${DB_PORT:-3306}" -- /tmp/run_install_prestashop.sh "${RUN_INSTALL}" "${APP_NAME}"
 fi
 
 # Run the main container command.
--- docker/nginx/docker_entrypoint.sh
+++ docker/nginx/docker_entrypoint.sh
@@ -4,8 +4,8 @@
 set -e
 
 # Wait for the nginx app to be up, if we know where it is.
-if [ -n "$NGINX_HOST" ]; then
-  /tmp/wait-for-it.sh --timeout=60 --strict "$NGINX_HOST:${NGINX_PORT:-9000}"
+if [ -n "$APP_HOST" ]; then
+  /tmp/wait-for-it.sh --timeout=60 --strict "$APP_HOST:${APP_PORT:-9000}"
 fi
 
 # Run the main container command.
