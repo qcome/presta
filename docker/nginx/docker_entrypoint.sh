c12c735563f58a2f05623fdac3c51de53288b5b9
Quentin <quentin.come@etu.univ-orleans.fr>
2021-03-13 22:13:43 +0100

    m

--- docker/nginx/docker_entrypoint.sh
+++ docker/nginx/docker_entrypoint.sh
@@ -7,7 +7,7 @@ echo "$@"
 
 # Wait for the nginx app to be up, if we know where it is.
 if [ -n "$APP_HOST" ]; then
-  /tmp/wait-for-it.sh --timeout=60 --strict "$APP_HOST:${APP_PORT:-9000}" -- "$RUN_INSTALL" "$APP_NAME"
+  /tmp/wait-for-it.sh --timeout=60 --strict "$APP_HOST:${APP_PORT:-9000}"
 fi
 
 # Run the main container command.
