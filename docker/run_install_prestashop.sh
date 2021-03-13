0f6ef424d0a8f33d414c02174ede3a992becea20
Quentin <quentin.come@etu.univ-orleans.fr>
2021-03-13 22:22:14 +0100

    m

--- docker/run_install_prestashop.sh
+++ docker/run_install_prestashop.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-if [ $1 = true ] ; then
-    echo "Running prestashop install..."
-    runuser -g www-data -u www-data -- php -d memory_limit=-1 /var/www/html/install/index_cli.php \
-        --domain=${PS_DOMAIN} \
-        --db_server=${MYSQL_SERVER} \
-        --db_name=${MYSQL_DATABASE} \
-        --db_user=${MYSQL_USER} \
-        --db_password=${MYSQL_PASSWORD} \
-        --email=${PS_ADMIN_EMAIL} \
-        --password=${PS_ADMIN_PASSWORD} \
-        --language=${PS_LANGUAGE} \
-        --country=${PS_COUNTRY} \
-        --db_create=${PS_DB_CREATE}
-    rm -r /var/www/html/install
-    mv /var/www/html/admin /var/www/html/${PS_FOLDER_ADMIN}
-    mv /tmp/phppsinfo.php /var/www/html
+if [ -z "$1" ]; then
+    echo "Boolean parameter for install missing"; 
+else
+    if [ $1 = true ] ; then
+        echo "Running prestashop install..."
+        runuser -g www-data -u www-data -- php -d memory_limit=-1 /var/www/html/install/index_cli.php \
+            --domain=${PS_DOMAIN} \
+            --db_server=${MYSQL_SERVER} \
+            --db_name=${MYSQL_DATABASE} \
+            --db_user=${MYSQL_USER} \
+            --db_password=${MYSQL_PASSWORD} \
+            --email=${PS_ADMIN_EMAIL} \
+            --password=${PS_ADMIN_PASSWORD} \
+            --language=${PS_LANGUAGE} \
+            --country=${PS_COUNTRY} \
+            --db_create=${PS_DB_CREATE}
+        rm -r /var/www/html/install
+        mv /var/www/html/admin /var/www/html/${PS_FOLDER_ADMIN}
+        mv /tmp/phppsinfo.php /var/www/html
+    fi
 fi
 # cd /var/www/html
 # chmod 600 /root/.ssh/id_rsa
