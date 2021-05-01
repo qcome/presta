af7bb9b9a82eec00fc917f0d2fb762dc7e2d1cc5
Quentin <quentin.come@etu.univ-orleans.fr>
2021-05-01 21:21:15 +0200

    m

--- prestashop.sh
+++ prestashop.sh
@@ -38,6 +38,6 @@ else
         chown www-data:www-data -R $app_folder/
     fi
 
-    docker-compose -f ./docker/docker-compose.yml --project-name presta-$app_name down
+    APP_NAME=$app_name RUN_INSTALL=$run_install docker-compose -f ./docker/docker-compose.yml --project-name presta-$app_name down
     APP_NAME=$app_name RUN_INSTALL=$run_install docker-compose -f ./docker/docker-compose.yml --project-name presta-$app_name up --build
 fi
