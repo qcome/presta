ae5ca030873402ca29b18a71395250cc5ddacefd
Quentin <quentin.come@etu.univ-orleans.fr>
2021-05-09 16:54:46 +0200

    m

--- docker/php-fpm/run_install_prestashop.sh
+++ docker/php-fpm/run_install_prestashop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Inside 'run_install_prestashop.sh'with parameter: $1 and $2"
+echo "Inside 'run_install_prestashop.sh' with parameter: $1"
 if [ -z "$1" ]; then
     echo "Boolean parameter for install missing"; 
 else
